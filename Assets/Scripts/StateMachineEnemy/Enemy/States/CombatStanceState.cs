bf60cad42504e01c6e9b9b1dd8b4bb36f0719cef
Mihajlo Kucak <kucakmihajloskola@gmail.com>
2022-02-20 12:50:28 +0100

    Combat Stance State is now smarter because it can avoid obstacles in its path...it has pathfinding

--- Assets/Scripts/StateMachineEnemy/Enemy/States/CombatStanceState.cs
+++ Assets/Scripts/StateMachineEnemy/Enemy/States/CombatStanceState.cs
@@ -13,38 +13,77 @@ namespace Isekai
         public float cTimer;
           public override State Tick(EnemyManager enemyManager,BaseStats enemyStats, EnemyAnimationHandler enemyAnimationHandler)
              {
-                enemyManager.navMeshAgent.enabled = false;
-                enemyAnimationHandler.enemyAnimator.SetBool("isInteracting", true);
-                enemyAnimationHandler.enemyAnimator.SetBool("isRecovering", false);
+                enemyManager.FaceTarget();
+                #region AnimatorBools
+                enemyAnimationHandler.enemyAnimator.SetBool("isInteracting", false);
+                enemyAnimationHandler.enemyAnimator.SetBool("combatIdle", true);
+
+                #endregion
+                #region NavMesh
+            enemyManager.navMeshAgent.speed = 2.4f;
+                enemyManager.navMeshAgent.SetDestination(enemyManager.currentTarget.transform.position);
+            #endregion
+
+                #region ChargeState 
                 cTimer += Time.deltaTime;
-                if(cTimer  > enemyManager.chargeTimer)
+                if (cTimer > enemyManager.chargeTimer)
+                {
+                  cTimer = 0;
+                  enemyAnimationHandler.enemyAnimator.SetBool("isCharging", true);
+                  return chargeState;
+                }
+            #endregion
+                #region AttackState
+             if (enemyManager.DistanceToEnemy() <= enemyManager.attackRange)
                 {
                    cTimer = 0;
-                   enemyAnimationHandler.enemyAnimator.SetBool("isCharging", true);
-                   return chargeState;
+                   enemyAnimationHandler.enemyAnimator.SetBool("attack", true);
+                   return attackState;
                 }
-                enemyManager.FaceTarget();
-                enemyAnimationHandler.enemyAnimator.SetBool("combatIdle", true);
-                enemyAnimationHandler.enemyAnimator.applyRootMotion = true;
-                float distance = Vector3.Distance(enemyManager.transform.position, enemyManager.currentTarget.transform.position);
-                if(distance <= enemyManager.attackRange)
+            #endregion
+                #region PursueTargetState
+             if (enemyManager.DistanceToEnemy() >= enemyManager.combatStanceStateRange)
                 {
-                cTimer = 0;
-                enemyAnimationHandler.enemyAnimator.SetBool("attack", true);
-                return attackState;
+                   cTimer = 0;
+                   enemyAnimationHandler.enemyAnimator.SetBool("combatIdle", false);
+                   enemyAnimationHandler.enemyAnimator.SetBool("isCharging", false);
+                   return pursueTargetState;
                 }
-
-                if(distance >= enemyManager.combatStanceStateRange)
-                {
-                cTimer = 0;
-                enemyAnimationHandler.enemyAnimator.SetBool("combatIdle", false);
-                enemyAnimationHandler.enemyAnimator.SetBool("isCharging", false);
-                return pursueTargetState;
-                }
-                return this;
+            #endregion
+            return this;
 
 
             // Conditions for ROOT MOTION ANIMATION ARE applyROOTMOTION, navmeshagent DISABLED,  ANIMATOR HAS TO HAVE ANIMATE PHYSICS and in no way shape or form should it have isKinematic ENABLED..........Ok Imma work on that.......LET'S FUCKING GOOOOOO
              }
     }
 }
+
+// OLD CODE
+//enemyAnimationHandler.enemyAnimator.SetBool("isInteracting", true);
+//enemyAnimationHandler.enemyAnimator.SetBool("isRecovering", false);
+//cTimer += Time.deltaTime;
+//if (cTimer > enemyManager.chargeTimer)
+//{
+//    cTimer = 0;
+//    enemyAnimationHandler.enemyAnimator.SetBool("isCharging", true);
+//    return chargeState;
+//}
+//enemyManager.FaceTarget();
+//enemyAnimationHandler.enemyAnimator.SetBool("combatIdle", true);
+//enemyAnimationHandler.enemyAnimator.applyRootMotion = true;
+//float distance = Vector3.Distance(enemyManager.transform.position, enemyManager.currentTarget.transform.position);
+//if (distance <= enemyManager.attackRange)
+//{
+//    cTimer = 0;
+//    enemyAnimationHandler.enemyAnimator.SetBool("attack", true);
+//    return attackState;
+//}
+
+//if (distance >= enemyManager.combatStanceStateRange)
+//{
+//    cTimer = 0;
+//    enemyAnimationHandler.enemyAnimator.SetBool("combatIdle", false);
+//    enemyAnimationHandler.enemyAnimator.SetBool("isCharging", false);
+//    return pursueTargetState;
+//}
+//return this;
